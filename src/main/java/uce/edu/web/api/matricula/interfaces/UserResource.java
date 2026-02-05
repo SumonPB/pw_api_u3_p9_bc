bde079ff6da8515c43653ed6ee52d6a444e17fcf
DavidLlumiquinga <byroncondolo99@gmail.com>
2026-02-05 18:43:38 -0500

    init

--- src/main/java/uce/edu/web/api/matricula/interfaces/UserResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/UserResource.java
@@ -3,7 +3,6 @@ package uce.edu.web.api.matricula.interfaces;
 import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
